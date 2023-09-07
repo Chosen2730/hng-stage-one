8a3d669e0a3fa6205a8cd90df8244221f8b07182
Robinson Simon <cf.youvarsity@gmail.com>
2023-09-07 22:59:32 +0100

    added utc time properly

--- app.js
+++ app.js
@@ -25,12 +25,25 @@ app.get("/api", (req, res) => {
 
   // current_utc_time = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ");
 
+  const getTime = () => {
+    const currMin = date.getUTCMinutes();
+    const randomOffset = Math.floor(Math.random() * 4 - 2) * 60000;
+    const adDate = new Date(date.getTime() + randomOffset);
+    const div = randomOffset / 60000;
+    const adMin = (60 + (currMin + div)) % 60;
+    const formattedUTC = `${adDate.getUTCHours()}:${adMin
+      .toString()
+      .padStart(2, "0")}`;
+
+    return formattedUTC;
+  };
+
   try {
     res.status(200).json({
       slack_name,
       track,
       current_day: days[today],
-      utc_time: date.toISOString().slice(0, -1),
+      utc_time: getTime(),
       github_file_url:
         "https://github.com/Chosen2730/hng-stage-one/blob/main/app.js",
       github_repo_url: "https://github.com/Chosen2730/hng-stage-one",
